1a10313879711683f53b22e559c216dfa3922289
Matthew Ferderber <matthewferderber@gmail.com>
2016-04-26 01:46:50 -0400

    Added help to commands and arguments, began storing users

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 "use strict";
-var Discord = require("discord.js");
-var Sounds = require('./lib/models/sounds.js');
-var Command = require("./lib/Command.js");
-var config = require('./config.js');
-var soundCommand = require("./lib/commands/SoundCommand.js");
-var playSoundCommand = require("./lib/commands/PlaySoundCommand.js");
-var cleanCommand = require("./lib/commands/CleanCommand.js");
-var imageCommand = require("./lib/commands/ImageCommand.js");
-var pullChanges = require("./lib/commands/PullChangesCommand.js");
-var mongoose = require("mongoose");
-var bot = new Discord.Client();
+var Discord = require("discord.js"),
+    Sounds = require('./lib/models/sounds.js'),
+    Command = require("./lib/Command.js"),
+    config = require('./config.js'),
+    soundCommand = require("./lib/commands/SoundCommand.js"),
+    playSoundCommand = require("./lib/commands/PlaySoundCommand.js"),
+    cleanCommand = require("./lib/commands/CleanCommand.js"),
+    imageCommand = require("./lib/commands/ImageCommand.js"),
+    helpCommand = require("./lib/commands/HelpCommand.js"),
+    profileCommand = require("./lib/commands/ProfileCommand.js"),
+    pullChanges = require("./lib/commands/PullChangesCommand.js"),
+    userListeners = require("./lib/userListeners.js"),
+    mongoose = require("mongoose"),
+    bot = new Discord.Client();
+
 mongoose.connect(config.db);
 
-var commands = [playSoundCommand, soundCommand, cleanCommand];
+var commands = [playSoundCommand, soundCommand, cleanCommand, profileCommand];
+userListeners(bot);
+
 bot.on("message", function(message) {
     if (message.content.charAt(0) === '!') {
         doCommand(message);
@@ -31,15 +37,20 @@ bot.loginWithToken(config.discordToken, function(err, token) {
 });
 
 function doCommand(message) {
-    commands.forEach(function(command) {
-        let space = message.content.indexOf(' ');
-        if (space === -1) {
-            space = message.content.length;
-        }
-        let cmdString = message.content.substring(1, space).toLowerCase();
-        if (cmdString === command.keyword ||
-            (command.aliases != null && command.aliases.some(a => a.toLowerCase() === cmdString ? true : false))) {
-            return command.execute(message).then(() => {}).catch(e => console.error(e));
-        }
-    });
+    if (/^!(help|h)$/.test(message.content))
+        helpCommand.execute(message, commands).then(((msg) => {
+            message.client.sendMessage(message.channel, msg);
+        }));
+    else
+        commands.forEach(function(command) {
+            let space = message.content.indexOf(' ');
+            if (space === -1) {
+                space = message.content.length;
+            }
+            let cmdString = message.content.substring(1, space).toLowerCase();
+            if (cmdString === command.keyword ||
+                (command.aliases != null && command.aliases.some(a => a.toLowerCase() === cmdString ? true : false))) {
+                return command.execute(message).then(() => {}).catch(e => console.error(e));
+            }
+        });
 }
