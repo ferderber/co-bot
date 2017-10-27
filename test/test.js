2c3e25fbcffbc4dc5d198fd0e8743fc7b6aca2ee
Matthew Ferderber <matthewferderber@gmail.com>
2017-10-26 21:35:21 -0400

    lint

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var assert = require('chai').assert;
 var soundCommand = require('../commands/sound.js');
-describe('Sound', function() {
-    describe('#', function() {
-        it('should return -1 when the value is not present', function() {
+describe('Sound', function () {
+    describe('#', function () {
+        it('should return -1 when the value is not present', function () {
             soundCommand.list().then(a => {
                 console.log(a);
                 assert.equals(true, true);
@@ -10,6 +10,6 @@ describe('Sound', function() {
         });
     });
 });
-describe('MessageCleaner', function() {
+describe('MessageCleaner', function () {
 
 });
